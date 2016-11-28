7192ca985caffbf3b5d17dce4c36dee689fbc717
Dinesh Adhithya <dineshadhithya@Dineshs-Mac.local>
2016-11-28 18:15:24 +0530

    Adf

--- studios/settings.py
+++ studios/settings.py
@@ -124,4 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR,'static_source/')
+
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'awestruck_app','static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static_source/')
